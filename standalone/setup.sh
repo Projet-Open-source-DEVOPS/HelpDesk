e0983465aa0270ce332caf0ea5538837a6158391
Lamiaa78 <100521392+Lamiaa78@users.noreply.github.com>
2023-11-30 11:17:49 +0100

    Update setup.sh

--- standalone/setup.sh
+++ standalone/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# if docker.env does not exist, create it from the template
+# if docker.env does not exist create it from the template
 if [ ! -f docker.env ]; then
     cp docker.env.template docker.env
-    echo "DJANGO_HELPDESK_SECRET_KEY=$(openssl rand -hex 32)" >> docker.env
-    echo "POSTGRES_PASSWORD=$(openssl rand -hex 32)" >> docker.env
 fi
+
+    echo "DJANGO_HELPDESK_SECRET_KEY=$(openssl rand -hex 32)" > docker.env
+    echo "POSTGRES_PASSWORD=$(openssl rand -hex 32)" >> docker.env
