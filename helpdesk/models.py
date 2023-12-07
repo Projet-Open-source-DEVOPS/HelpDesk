0ad3dface611490a2cee32d221ca9db03ab886fd
Thomas Soutif <th.soutif@gmail.com>
2023-12-07 17:44:02 +0100

    Add new permission model for the user
    for the ticket

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -2089,3 +2089,13 @@ class ChecklistTask(models.Model):
 
     def __str__(self):
         return self.description
+
+
+class UserPermissions(models.Model):
+    class Meta:
+        permissions = (
+            ("user_can_view_all_tickets", "User can view all tickets"),
+            ("user_can_view_own_tickets", "User can view own tickets"),
+            ("user_can_view_tickets_where_assigned", "User can view the tickets where he is assigned."),
+
+        )
